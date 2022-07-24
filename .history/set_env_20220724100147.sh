f45c8798d646f58f740dddc84b465c21886993eb
ValGrace <gracevalerie1@gmail.com>
2022-07-24 13:47:50 +0300

    exposed deployments

---
+++ .history/set_env_20220724100147.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=microproject322
+export POSTGRES_HOST=proj3-db.cesbaijnry8m.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=micros-bucket-proj322
+export AWS_REGION=us-west-2
+export AWS_PROFILE=clusterprofile
+export JWT_SECRET=testing
+export URL=http://localhost:8100
