76eddc5ba94d358d67949ae25eaed29ac33e71b0
ValGrace <gracevalerie1@gmail.com>
2022-07-29 18:56:12 +0300

    nginx proxy

---
+++ .history/set_env_20220727231927.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=microproject322
+export POSTGRES_HOST=proj3-db.cvkqhhy37gb9.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=micro-bucket-proj3
+export AWS_REGION=us-east-2
+export AWS_PROFILE=newprofile
+export JWT_SECRET=testing
+export URL=http://localhost:8100
